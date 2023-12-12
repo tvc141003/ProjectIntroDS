23cb686a79670c03e021add8fa4c17eb4cf3e550
Nguyen Tuan Thanh <ntt.thanh0504@gmail.com>
2023-12-12 14:53:28 +0700

    Complete all file and change some thing

--- DS_Project/reports/build.sh
+++
@@ -1,8 +0,0 @@
-pandoc "./report.md" "./metadata.yaml" \
-    --output="./report.pdf" \
-    --from markdown \
-    --template="./eisvogel.tex" \
-    --number-sections \
-    --resource-path="./" \
-    --listings \
-    --pdf-engine=xelatex
